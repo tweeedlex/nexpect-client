bc49b3ffbf0970520bcf700614f3d30d6e8235dd
krutiy <7545245@ukr.net>
2024-01-27 21:53:08 +0200

    added 2 layer triangles

--- src/components/ScrollView/ScrollView.jsx
+++ src/components/ScrollView/ScrollView.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import styles from "./ScrollView.module.scss";
 import triangleImage from "../../img/polygon.png";
 import arrowDownImage from "../../img/arrow-down.png";
+import smallTriangleImage from "../../img/triangleSmall.png";
 import chevronImage from "../../img/chevron.png";
 
 const ScrollView = ({ active }) => {
@@ -24,6 +25,66 @@ const ScrollView = ({ active }) => {
               </div>
             </div>
             <img className={styles.chevron} src={chevronImage} alt="" />
+          </div>
+          <div className={styles.backgroundLayerTop}>
+            {[...Array(40)].map((_, i) => {
+              const positionTop = Math.floor(Math.random() * 100);
+              const positionLeft = Math.floor(Math.random() * 100);
+              const center = 50;
+              const distanceFromCenter =
+                Math.abs(center - positionTop) +
+                Math.abs(center - positionLeft);
+
+              const scale = 1 - distanceFromCenter / 100 + 0.1;
+              const rotate = Math.floor(Math.random() * 360);
+
+              const minDistance = Math.min(
+                positionTop,
+                positionLeft,
+                100 - positionTop,
+                100 - positionLeft
+              );
+
+              let animationDirection;
+              switch (minDistance) {
+                case positionTop:
+                  animationDirection = "top";
+                  break;
+                case positionLeft:
+                  animationDirection = "left";
+                  break;
+                case 100 - positionTop:
+                  animationDirection = "bottom";
+                  break;
+                case 100 - positionLeft:
+                  animationDirection = "right";
+                  break;
+                default:
+                  animationDirection = "top";
+                  break;
+              }
+
+              return (
+                <img
+                  key={`${positionTop + positionLeft}${i}`}
+                  src={smallTriangleImage}
+                  className={
+                    styles.smallTriangle +
+                    " " +
+                    styles[`small-${animationDirection}`]
+                  }
+                  alt=""
+                  style={{
+                    top: `${positionTop}%`,
+                    left: `${positionLeft}%`,
+                    transform: `rotate(${rotate}deg) scale(${scale})`,
+                    animationDuration: `${
+                      Number((Math.random() * 2).toFixed(2)) + 1
+                    }s`,
+                  }}
+                />
+              );
+            })}
           </div>
           <div className={styles.background}>
             {[...Array(19)].map(() => (

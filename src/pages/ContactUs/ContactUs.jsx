60c9132436f4b503ef30e2cc5cd6b452e73ddeaf
krutiy <7545245@ukr.net>
2024-02-04 21:14:58 +0200

    added adaptive to contactus page, animations on buttons

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ContactUs.module.scss";
 import chevronTopImage from "../../img/chevron-top.png";
 import instagramImage from "../../img/instagram.png";
@@ -10,9 +10,25 @@ import mailImage from "../../img/mail-gray.png";
 import { Link } from "react-router-dom";
 
 const ContactUs = () => {
+  const [tooltip, setTooltip] = useState({ show: false, x: 0, y: 0 });
+
   useEffect(() => {
     document.body.style.backgroundColor = "#161616";
   }, []);
+
+  let timeout = null;
+
+  const copyToClipboard = (text, event) => {
+    if (tooltip.show) return;
+    navigator.clipboard.writeText(text);
+    const { clientX, clientY } = event;
+    setTooltip({ show: true, x: clientX, y: clientY });
+    clearTimeout(timeout);
+    timeout = setTimeout(
+      () => setTooltip({ show: false, x: clientX, y: clientY }),
+      2000
+    );
+  };
 
   return (
     <div className={styles.page}>
@@ -59,30 +75,58 @@ const ContactUs = () => {
           <div className={styles.block}>
             <h2>Contact us in a way comfortable for you!</h2>
             <div className={styles.contacts}>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("@telegram", e)}
+              >
                 <img src={telegramImage} alt="" />
                 <p>@telegram</p>
               </div>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("@messenger", e)}
+              >
                 <img src={messengerImage} alt="" />
                 <p>@messenger</p>
               </div>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("email@domain.com", e)}
+              >
                 <img src={mailImage} alt="" />
                 <p>email@domain.com</p>
               </div>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("@instagram", e)}
+              >
                 <img src={instagramImage} alt="" />
                 <p>@instagram</p>
               </div>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("+8918239898", e)}
+              >
                 <img src={whatsappImage} alt="" />
                 <p>+8918239898</p>
               </div>
-              <div className={styles.contact}>
+              <div
+                className={styles.contact}
+                onClick={(e) => copyToClipboard("+8918239898", e)}
+              >
                 <img src={viberImage} alt="" />
                 <p>+8918239898</p>
               </div>
+            </div>
+            <div
+              className={styles.tooltip}
+              style={{
+                top: `${tooltip.y}px`,
+                left: `${tooltip.x}px`,
+                opacity: tooltip.show ? 1 : 0,
+              }}
+            >
+              Copied!
             </div>
           </div>
         </div>
